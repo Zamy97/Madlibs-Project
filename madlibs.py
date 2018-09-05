81c283e4eae38621332ab00c6a0028ff7d886fee
Akhtar Zaman <auz.zamy@gmail.com>
2018-09-05 16:15:45 -0700

    test

---
+++ madlibs.py
@@ -0,0 +1,14 @@
+#This is the madlibs project that I have to create where the users will type in words to write their story.
+
+your_name = input("Enter your name: ")
+continuous_verb = input("Enter a continus verb: ")
+scared_not_scared = input("Are you scared or not scared: ")
+kitchen_item = input("Enter a kitchen equipment name:")
+country_name = input("Enter a country name: ")
+celebraity = input("Enter a celebraity name: ")
+he_or_she = input("are you he or she? ")
+
+
+Story = your_name + "loves everything about " +  continuous_verb +  "she runs evryday to stay healthy. She's not " + scared_not_scared  + " of lions at all. She loves everything about " + kitchen_item + "she wants to go to" + country_name + " to meet " + celebraity + "in person". "overall " + he_or_she + "is really happy with " + he_or_she +  " life, but deep down inside" + he_or_she + "will be upset if" + he_or_she + "doesn't get to meet " + celebraity + "in person"
+
+print(story)
