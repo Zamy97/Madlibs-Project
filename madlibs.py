d30193c95663c3975e847de70924d4716023ebe2
Akhtar Zaman <auz.zamy@gmail.com>
2018-09-05 22:39:38 -0700

    Test

--- madlibs.py
+++ madlibs.py
@@ -9,6 +9,6 @@ celebraity = input("Enter a celebraity name: ")
 he_or_she = input("are you he or she? ")
 
 
-Story = your_name + "loves everything about " +  continuous_verb +  "she runs evryday to stay healthy. She's not " + scared_not_scared  + " of lions at all. She loves everything about " + kitchen_item + "she wants to go to" + country_name + " to meet " + celebraity + "in person". "overall " + he_or_she + "is really happy with " + he_or_she +  " life, but deep down inside" + he_or_she + "will be upset if" + he_or_she + "doesn't get to meet " + celebraity + "in person"
+story_madlibs = your_name + "loves everything about " +  continuous_verb + he_or_she +  " runs evryday to stay healthy." + he_or_she + scared_not_scared  + " of lions at all. " + he_or_she + " loves everything about " + kitchen_item + he_or_she " wants to go to" + country_name + " to meet " + celebraity + "in person". "overall " + he_or_she + "is really happy with " + he_or_she +  " life, but deep down inside" + he_or_she + "will be upset if" + he_or_she + "doesn't get to meet " + celebraity + "in person"
 
-print(story)
+print(story_madlibs)
